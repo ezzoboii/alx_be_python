b87a59f3e983391fcc45d9e4989b109eefca6726
Ezrah <ezrahpietersen309@gmail.com>
2024-06-21 12:01:41 -0700

    made changes to basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,3 +3,6 @@ number2 = 5
 print("addition","of",number1,"and",number2,"is",15)
 print("subtraction","of",number1,"and",number2,"is",5)
 print("multiplication","of",number1,"and",number2,"is",50)
++
+-
+*
