fdf8523c43daf699630c851d496d14339f9392d4
Ezrah <ezrahpietersen309@gmail.com>
2024-06-21 12:07:09 -0700

    made chnages to file

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,5 @@
 number1 = 10
 number2 = 5
-print("addition","of",number1,"and",number2,"is",15)
-print("subtraction","of",number1,"and",number2,"is",5)
-print("multiplication","of",number1,"and",number2,"is",50)
-+
--
-*
+print("addition","of",number1,"+",number2,"is",15)
+print("subtraction","of",number1,"-",number2,"is",5)
+print("multiplication","of",number1,"*",number2,"is",50)
