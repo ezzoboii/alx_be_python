d549d1c4168b6fd2dc9fd07e1f268c65adf2ba47
Ezrah <ezrahpietersen309@gmail.com>
2024-07-07 07:46:40 -0700

    added command to temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -4,8 +4,8 @@ CELSIUS_TO_FAHRENHEIT_FACTOR
 # temp_conversion_tool.py
 
 # Define Global Conversion Factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/ 5
 
 # Conversion Functions
 def convert_to_celsius(fahrenheit):
